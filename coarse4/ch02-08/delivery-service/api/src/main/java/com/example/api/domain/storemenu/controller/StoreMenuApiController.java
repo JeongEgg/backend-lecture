ab1cf0c774f2cc5ca2c5ed6e55552343ac64a127
JeongBanSeok <wibalislfkas@naver.com>
2024-10-22 21:30:23 +0900

    스토어메뉴 비즈니스 로직 개발

---
+++ coarse4/ch02-08/delivery-service/api/src/main/java/com/example/api/domain/storemenu/controller/StoreMenuApiController.java
@@ -0,0 +1,29 @@
+package com.example.api.domain.storemenu.controller;
+
+
+import com.example.api.common.api.Api;
+import com.example.api.domain.storemenu.business.StoreMenuBusiness;
+import com.example.api.domain.storemenu.controller.model.StoreMenuResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/store-menu")
+public class StoreMenuApiController {
+
+    private final StoreMenuBusiness storeMenuBusiness;
+
+    @GetMapping("/search")
+    public Api<List<StoreMenuResponse>> search(
+            @RequestParam Long storeId
+    ){
+        var response = storeMenuBusiness.search(storeId);
+        return Api.OK(response);
+    }
+}
