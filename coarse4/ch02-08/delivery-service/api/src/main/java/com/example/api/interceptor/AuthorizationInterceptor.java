244a54148ed4fc906de820dc01cd354325307b67
JeongBanSeok <wibalislfkas@naver.com>
2024-10-22 15:17:45 +0900

    토큰 발행에 대한 비즈니스 로직 작성 2

--- coarse4/ch02-08/delivery-service/api/src/main/java/com/example/api/interceptor/AuthorizationInterceptor.java
+++ coarse4/ch02-08/delivery-service/api/src/main/java/com/example/api/interceptor/AuthorizationInterceptor.java
@@ -1,13 +1,21 @@
 package com.example.api.interceptor;
 
+import com.example.api.common.error.ErrorCode;
+import com.example.api.common.error.TokenErrorCode;
+import com.example.api.common.exception.ApiException;
+import com.example.api.domain.token.business.TokenBusiness;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,6 +25,9 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
     /*
     * 인터셉터 구현 - uri 로깅. 요청에 대한 통과여부. 리소스를 요청하는 경우 통과여부.
     * */
+
+    private final TokenBusiness tokenBusiness;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("Authorization Interceptor url : {}", request.getRequestURI());
@@ -31,8 +42,21 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
             return true;
         }
 
-        // TODO header 검증
+        // 인터셉터에서 토큰 인증 처리
+        var accessToken = request.getHeader("authorization-token");
+        if(accessToken == null){
+            throw new ApiException(TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND);
+        }
 
-        return true;    // 일단 통과 처리
+        var userId = tokenBusiness.validationAccessToken(accessToken);
+
+        if(userId != null){
+            var requestContext = Objects.requireNonNull(RequestContextHolder.getRequestAttributes());
+            requestContext.setAttribute("userId", userId, RequestAttributes.SCOPE_REQUEST);
+            return true;
+        }
+
+
+        throw new ApiException(ErrorCode.BAD_REQUEST, "인증실패");
     }
 }
